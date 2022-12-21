c259f17aff6064f3c487dbc2d7ceff151b2654b1
isaac-silversat <isaac@silversat.org>
2022-12-21 17:43:37 -0500

    Fixed an error in serial1 data send
    Line 65 {if (serialBufPos > 0)} was changed to {if (serial1BufPos > 0)}.
    
    The code was tested on a pair of Mega 2560 boards
    
    To Do: Implement a check which prevents data transmission when a receilve line is busy.

--- Arduino/serial_data_repeater/serial_data_repeater.ino
+++ Arduino/serial_data_repeater/serial_data_repeater.ino
@@ -8,20 +8,14 @@
 #include <CircularBuffer.h>
 
 // Constants
-const int SERIAL_SPEED = 9600;
-const int BUFFERSIZE = 256;
-
-// Define variables
-unsigned char serialBuffer[BUFFERSIZE];
-unsigned char serial1Buffer[BUFFERSIZE];
-unsigned int serial1BufPos = 0; // Array pointer for serial1Buffer
-unsigned int serialBufPos = 0;  // Array pointer for serialBuffer
-char inByte = '\0';
+const unsigned int SERIAL_SPEED = 9600;
+const unsigned int BUFFERSIZE = 256;
 
 void setup()
 {
     // put your setup code here, to run once:
 
+    // Open serial ports
     Serial.begin(SERIAL_SPEED);
     Serial1.begin(SERIAL_SPEED);
 }
@@ -30,37 +24,50 @@ void loop()
 {
     // put your main code here, to run repeatedly:
 
+    // Declare variables
+    CircularBuffer<char, BUFFERSIZE> serialBuffer;
+    CircularBuffer<char, BUFFERSIZE> serial1Buffer;
+    unsigned int serial1BufPos = 0; // Array pointer for seria1lArray
+    unsigned int serialBufPos = 0;  // Array pointer for serialArray
+    char inByte = '\0';
+
     // read from port 0 and put the incoming data into a buffer
     while (Serial.available() > 0)
     {
         inByte = Serial.read();
-        serialBuffer[serialBufPos] = inByte;
+        serialBuffer.push(inByte);
 
         // Record the last index position of the buffer
-        serialBufPos = serialBufPos + 1;
+        serialBufPos++;
     }
 
     // read from port 1 and put the incoming data into a buffer
     while (Serial1.available() > 0)
     {
         inByte = Serial1.read();
-        serial1Buffer[serial1BufPos] = inByte;
+        serial1Buffer.push(inByte);
 
         // Record the last index position of the buffer
-        serial1BufPos = serial1BufPos + 1;
+        serial1BufPos++;
     }
 
+    // Write serialBuffer to serial1
     if (serialBufPos > 0)
     {
-        Serial1.write(serialBuffer, serialBufPos);
-        serialBufPos = 0;
-        memset(serialBuffer, 0, BUFFERSIZE);
+        // Send and erase serialBuffer
+        for (unsigned int i = 0; i < serialBufPos; i++)
+            Serial1.write(serialBuffer[i]);
+        serialBufPos = 0; // Reset the array index
+        serialBuffer.clear();
     }
 
+    // Write serial1Buffer to serial
     if (serial1BufPos > 0)
     {
-        Serial.write(serial1Buffer, serial1BufPos);
-        serial1BufPos = 0;
-        memset(serial1Buffer, 0, BUFFERSIZE);
+        // Send and erase serial1Buffer
+        for (unsigned int i = 0; i < serial1BufPos; i++)
+            Serial.write(serial1Buffer[i]);
+        serial1BufPos = 0; // Reset the array index
+        serialBuffer.clear();
     }
 }
