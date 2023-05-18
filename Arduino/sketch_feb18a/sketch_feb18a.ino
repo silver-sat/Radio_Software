a3dccec2d927a9b6d674b17980acf1196d6c6177
isaac-silversat <isaac@silversat.org>
2023-05-18 11:31:05 -0400

    Unfinished class implementation in-progress

--- Arduino/sketch_feb18a/sketch_feb18a.ino
+++ Arduino/sketch_feb18a/sketch_feb18a.ino
@@ -12,142 +12,130 @@
 #include <CircularBuffer.h>
 
 // Buffer Size
-const unsigned int BUFFERSIZE = 1024;           // bytes
-const unsigned int HARDWARESERIALSPEED = 57600; // baud
-const unsigned int PACKETSIZE = BUFFERSIZE;     // Could be the AX5043 FIFO size
+const unsigned int BUFFERSIZE{1024};           // bytes
+const unsigned int HARDWARESERIALSPEED{57600}; // baud
+const unsigned int RADIOSPEED{9600};           // baud
+const unsigned int PACKETSIZE{BUFFERSIZE};     // Could be the AX5043 FIFO size
 
 // Circular Buffer
 CircularBuffer<char, BUFFERSIZE> serialBuffer;
 
-// Structures
+// Classes
 // This hold the data and command byte of an unencoded KISS packet
 class KISSPacket
 {
+private:
+    unsigned int index = 0; // Array index (shared between size() and decapsulate())
 public:
+    CircularBuffer<char, PACKETSIZE> packet; // Cut KISS packet
+    unsigned int packetsize{0};              // Packet size
+    bool packetfound{false};                 // Whether a packet was found
+    char command{CMD_DATA};                  // KISS command byte
+
+    /* Assumed cases:
+
+        Case 1: A full and complete KISS packet exists in the buffer, possibly
+                preceded by noise. This may be detected by checking for two FENDs
+                seperated by at least a command byte.
+
+            Case 1a: Multiple full and complete KISS packets are in the buffer.
+                    Each packet is to be detected individually; processKISS shall
+                    run in a loop until the buffer is empty.
+
+        Case 2: Partial packets; these will be ignored.
+
+        Case 3: Command packets; packets which solely carry a command byte. For
+                every FEND detected, processKISS shall check the next byte. If the
+                next byte is a FEND, processKISS shall move to it and repeat the
+                check.
+
+                Commands are defined in KISS.h; however, not all of these are
+                supported. These ignored commands are those the KISS protocol lists
+                as optional. In addition, the following required commands are
+                ignored.
+
+                    CMD_DATA: Any following characters, to the next FEND, are
+                            assumed to be data to be sent to the serial port.
+                            When this data is copied to KISSPacket, the FENDS are
+                            ignored, and escape sequences are converted to their
+                            normal forms.
+
+                    CMD_TXDELAY: These are ignored because P-persistant CSMA is not
+                    CMD_PERSIST: implemented. This is because the link is assumed
+                    CMD_SLOTTIME: to include at most three peers on each board.
+
+                    CMD_DUPLEX: SilverSat's current hardware does not support full
+                                duplex.
+
+                    CMD_HARDWARE: SilverSat deprecated this in favour of its own
+                                custom hardware commands, to take advantage of
+                                the wide range of command bytes not defined by
+                                the KISS protocol.
+
+                    */
+
+    unsigned int size()
     {
-        CircularBuffer<char, PACKETSIZE> packet; // Unencoded packet data
-        unsigned int packetsize { 0 }            // Packet size
-        bool packetfound = false;                // Whether a packet was found
-        char command = CMD_DATA;                 // KISS command byte
-
-        unsigned int index = 0; // Array index (shared between size() and decapsulate())
-
-        /* Assumed cases:
-
-            Case 1: A full and complete KISS packet exists in the buffer, possibly
-                    preceded by noise. This may be detected by checking for two FENDs
-                    seperated by at least a command byte.
-
-                Case 1a: Multiple full and complete KISS packets are in the buffer.
-                        Each packet is to be detected individually; processKISS shall
-                        run in a loop until the buffer is empty.
-
-            Case 2: Partial packets; these will be ignored.
-
-            Case 3: Command packets; packets which solely carry a command byte. For
-                    every FEND detected, processKISS shall check the next byte. If the
-                    next byte is a FEND, processKISS shall move to it and repeat the
-                    check.
-
-                    Commands are defined in KISS.h; however, not all of these are
-                    supported. These ignored commands are those the KISS protocol lists
-                    as optional. In addition, the following required commands are
-                    ignored.
-
-                        CMD_DATA: Any following characters, to the next FEND, are
-                                assumed to be data to be sent to the serial port.
-                                When this data is copied to KISSPacket, the FENDS are
-                                ignored, and escape sequences are converted to their
-                                normal forms.
-
-                        CMD_TXDELAY: These are ignored because P-persistant CSMA is not
-                        CMD_PERSIST: implemented. This is because the link is assumed
-                        CMD_SLOTTIME: to include at most three peers on each board.
-
-                        CMD_DUPLEX: SilverSat's current hardware does not support full
-                                    duplex.
-
-                        CMD_HARDWARE: SilverSat deprecated this in favour of its own
-                                    custom hardware commands, to take advantage of
-                                    the wide range of command bytes not defined by
-                                    the KISS protocol.
-
-                        */
-
-        unsigned int size()
+        // Search the data for a FEND and save its index
+        while (packet[0] != FEND)
         {
-            // Search the data for a FEND and save its index
-            while (data[0] != FEND)
+            index++; // Increment the starting index
+        }
+
+        if (packet.size() > 0)
+        {
+            // Check packet.command and take appropriate action for local commands
+            while (packet[1] == FEND) // ignore the next FEND
             {
-                index++; // Increment the starting index
+                index++; // increment the index
             }
 
-            if (data.size() > 0)
+            // TODO: Move to a packet processor
+            // //  Get the packet command
+            // packet.command = data[1];
+
+            // In each of these cases, the next byte contains the respective data to store
+            // else if (packet.command == CMD_TXDELAY)
+            //     packet.txdelay = data[index + 1];
+            // else if (packet.command == CMD_PERSIST)
+            //     packet.P = data[index + 1];
+            // else if (packet.command == CMD_SLOTTIME)
+            //     packet.slottime = data[index + 1];
+
+            // Search for the next FEND
+            for (index; (packet[index] != FEND); index++)
+                packetsize++;
+
+            return packetsize;
+        }
+    }
+
+    // (2) Separate the packet size and processing routines. Avoid blocking code! */
+    // Functions
+    // Process a KISS packet
+    // Input: CircularBuffer data, KISSPacket to fill
+    // Return: nothing
+    // Comment: If the function did not find a KISS packet or command, packet.command will be less than 0.
+    // Warning: This function only extracts one packet per run. To extract another packet, run it again.
+    void decapsulate()
+    {
+        // Cut the
+
+        // Copy this packet to KISSPacket packet only if another FEND was found
+        if (nextFEND <= 1)
+        {
+            // Copy data to packet.packet from back to front (to avoid shifting {CircularBuffer data}, for speed)
+            for (unsigned int i = NEXTFEND; (i >= 0) & (NEXTFEND < PACKETSIZE); i++)
             {
-                // Check packet.command and take appropriate action for local commands
-                while (data[1] == FEND) // ignore the next FEND
-                {
-                    index++; // increment the index
-                }
-
-                // TODO: Move to a packet processor
-                // //  Get the packet command
-                // packet.command = data[1];
-
-                // In each of these cases, the next byte contains the respective data to store
-                // else if (packet.command == CMD_TXDELAY)
-                //     packet.txdelay = data[index + 1];
-                // else if (packet.command == CMD_PERSIST)
-                //     packet.P = data[index + 1];
-                // else if (packet.command == CMD_SLOTTIME)
-                //     packet.slottime = data[index + 1];
-
-                // Search for the next FEND
-                for (index; (data[nextFEND] != FEND); index++)
-                    packetsize++;
-
-                return packetsize;
+                packet[i] = packet.pop();
             }
+        }
 
-            // (2) Separate the packet size and processing routines. Avoid blocking code! */
-            // Functions
-            // Process a KISS packet
-            // Input: CircularBuffer data, KISSPacket to fill
-            // Return: nothing
-            // Comment: If the function did not find a KISS packet or command, packet.command will be less than 0.
-            // Warning: This function only extracts one packet per run. To extract another packet, run it again.
-            void decapsulate()
-            {
-                // Trim excess bytes off
-                for (unsigned int i = 0; i < index; i++)
-                {
-                    packet.shift()
-                }
-                for (unsigned int i = packet.size(); packet[i] != FEND; i--)
-                {
-                    // TODO: Return to this. I am not sure if I wrote it right
-                }
-                
-                
-                // Convert nextFEND to a constant for the following for loop
-                const unsigned int NEXTFEND = nextFEND;
-
-                // Copy this packet to KISSPacket packet only if another FEND was found
-                if (nextFEND <= 1)
-                {
-                    // Copy data to packet.packet from back to front (to avoid shifting {CircularBuffer data}, for speed)
-                    for (unsigned int i = NEXTFEND; (i >= 0) & (NEXTFEND < PACKETSIZE); i++)
-                    {
-                        packet.packet[i] = data.pop();
-                    }
-                }
-            }
-            else // in case 2, clear the buffer and set data to indicate such
-            {
-                data.clear();
-                packet.packetfound = false;
-                packet.command = -1;
-            }
+        else // in case 2, clear the buffer and set data to indicate such
+        {
+            packet.clear();
+            packetfound = false;
+            command = -1;
         }
     }
 };
