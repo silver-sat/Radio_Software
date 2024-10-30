7a741b23a2afc330f2a77bba07f73311631c2e30
tconrad26 <tconrad@arcaneideas.com>
2024-10-30 16:06:37 -0400

    this is the working version
    current working version.  Moved writes to just once per packet instead of leaving the file open
    fixed args error
    added code to handle keyboard interrupt

--- gnuradio/ssdv_file_capture.py
+++ gnuradio/ssdv_file_capture.py
@@ -4,37 +4,37 @@ import serial
 import argparse
 
 def main(args):
-    with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+    with serial.Serial('/dev/ttyS0', 9600) as ser:
         print(ser.name)
         packet = []
         packet_count = 0
-        with open("filename", "wb") as file:
-            while True:
-                x = ser.read(1)  # looking for C0
-                # print(x)
-                if x == b'\xc0':  # start of a packet detected
-                    dest = ser.read(1)  # read the destination (temp)
-                    if args.verbose:
-                        print("dest = ", dest)
-                    packet = bytearray(b'')
-                    read_byte = None
-                    while read_byte != b'\xc0':  # keep reading bytes until next 0xc0 encountered
-                        read_byte = ser.read(1)  # need to kiss decode, do it on the fly
-                        if read_byte == b'\xDB':
-                            read_next_byte = ser.read(1)
-                            if read_next_byte == b'\xDC':
-                                packet.extend(b'\xC0')
-                            elif read_next_byte == b'\xDD':
-                                packet.extend(b'\xDB')
-                        elif read_byte == b'\xc0':
-                            pass
-                        else:
-                            packet.extend(read_byte)
+        while True:
+            x = ser.read(1)  # looking for C0
+            # print(x)
+            if x == b'\xc0':  # start of a packet detected
+                dest = ser.read(1)  # read the destination (temp)
+                if args.verbose:
+                    print("dest = ", dest)
+                packet = bytearray(b'')
+                read_byte = None
+                while read_byte != b'\xc0':  # keep reading bytes until next 0xc0 encountered
+                    read_byte = ser.read(1)  # need to kiss decode, do it on the fly
+                    if read_byte == b'\xDB':
+                        read_next_byte = ser.read(1)
+                        if read_next_byte == b'\xDC':
+                            packet.extend(b'\xC0')
+                        elif read_next_byte == b'\xDD':
+                            packet.extend(b'\xDB')
+                    elif read_byte == b'\xc0':
+                        pass
+                    else:
+                        packet.extend(read_byte)
 
-            if arg.verbose:
+            if args.verbose:
                 print(packet)
                 print(len(packet))
-            file.write(packet)
+            with open(args.output, "ab") as file:  #append to the file
+                file.write(packet)
             packet_count += 1
             packet = []  # clear it
             print(f'count = {packet_count}')
@@ -48,4 +48,7 @@ if __name__ == "__main__":
     if args.output == None:
         print("output file required")
         quit()
-    main(args)
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt. Closing.")
